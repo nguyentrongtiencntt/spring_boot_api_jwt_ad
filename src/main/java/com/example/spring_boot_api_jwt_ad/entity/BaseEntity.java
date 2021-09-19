b08d7ddff1e6cee26ebcd04af71174a937e62513
Nguyễn Trọng Tiến <nguyentrongtiencntt@gmail.com>
2021-09-19 09:03:03 +0700

    finish update createBy, lastmodifyBy

--- src/main/java/com/example/spring_boot_api_jwt_ad/entity/BaseEntity.java
+++ src/main/java/com/example/spring_boot_api_jwt_ad/entity/BaseEntity.java
@@ -3,7 +3,9 @@ package com.example.spring_boot_api_jwt_ad.entity;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -31,7 +33,10 @@ public abstract class BaseEntity implements Serializable {
     @LastModifiedDate
     private Date updatedAt;
 
+    @CreatedBy
     private Long createdBy;
 
+    @LastModifiedBy
     private Long updatedBy;
+
 }
